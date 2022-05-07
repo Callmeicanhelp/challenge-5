ee6738128142067bbd05283cb54d62f15e691e6c
Daniel Warren II <callmeicanhelp@gmail.com>
2022-05-07 12:12:39 -0400

    initial schedule build

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,61 @@
 const time_div = $("#currentDay")
-
 time_div.text (moment ().format ("dddd, MMMM DO, m"));
 
+const nineAm = $("#9am")
+const tenAm = $("#10am")
+const elevenAm = $("#11am")
+const twelvePm = $("#12pm")
+const onePm = $("#1pm")
+const twoPm = $("#2Pm")
+const threePm = $("#3pm")
+const fourPm = $("#4pm")
+const fivePm = $("#5pm")
+
+var timeArray = [
+    nineAm,
+    tenAm,
+    elevenAm,
+    twelvePm,
+    onePm,
+    twoPm,
+    threePm,
+    fourPM,
+    fivePM
+]
+
+
 let textArea=$(".text-area")
-console.log(textArea.val());
+// console.log(textArea.val());
 
 let button = $("#save_button")
-button.on("click", function(){
-    console.log("button click")
-    localStorage.setItem(1, textArea.val())
-});
 
 function getFromLocalstorage() {
-    textArea.text = localstorage.getItem("1")
+    textArea.text = localstorage.getItem("")
 }
-getFromLocalstorage();
+
+function init() {
+    getFromLocalstorage()
+
+for (let i = 0; i < timeArray.length; i++) {
+    const element = timeArray[i];
+    //console.log(element);
+    $(element).on("click", function(event){
+        //console.log(event.target.className);
+        if(event.target.className == "col-mb-2 save_button") {
+            //console.log("click");
+            var eventBlock = $(event.target).siblings()
+            //console.log(eventBlock[1].value)
+            var timeKey = $(event.target).parent().parent().attr("id")
+            // console.log(timeKey)
+            localStorage.setItem(timeKey, eventBlock[1].value)
+        }
+    })
+    // $("#" + timeKey).children().children().siblings();
+    console.log($("#" + timeKey).children().children().siblings())
+    localStorage.getItem(timeKey)
+
+    //use timeArray instead of timeKey
+}
+}
+
+init()
