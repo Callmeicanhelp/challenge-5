8781d98ba3fc392e6bd27ee4c27234b7a61e2a86
Daniel Warren II <callmeicanhelp@gmail.com>
2022-05-08 21:54:54 -0400

    updated styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,77 @@
-const time_div = $("#currentDay")
-time_div.text (moment ().format ("dddd, MMMM DO, m"));
-
-const nineAm = $("#9am")
-const tenAm = $("#10am")
-const elevenAm = $("#11am")
-const twelvePm = $("#12pm")
-const onePm = $("#1pm")
-const twoPm = $("#2Pm")
-const threePm = $("#3pm")
-const fourPm = $("#4pm")
-const fivePm = $("#5pm")
-
-var timeArray = [
-    nineAm,
-    tenAm,
-    elevenAm,
-    twelvePm,
-    onePm,
-    twoPm,
-    threePm,
-    fourPM,
-    fivePM
-]
+// Display today's day and date
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
 
-let textArea=$(".text-area")
-// console.log(textArea.val());
-
-let button = $("#save_button")
-
-function getFromLocalstorage() {
-    textArea.text = localstorage.getItem("")
-}
-
-function init() {
-    getFromLocalstorage()
-
-for (let i = 0; i < timeArray.length; i++) {
-    const element = timeArray[i];
-    //console.log(element);
-    $(element).on("click", function(event){
-        //console.log(event.target.className);
-        if(event.target.className == "col-mb-2 save_button") {
-            //console.log("click");
-            var eventBlock = $(event.target).siblings()
-            //console.log(eventBlock[1].value)
-            var timeKey = $(event.target).parent().parent().attr("id")
-            // console.log(timeKey)
-            localStorage.setItem(timeKey, eventBlock[1].value)
-        }
+$(document).ready(function () {
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the textarea in JQuery
+        var text = $(this).siblings(".textarea").val();
+        var time = $(this).siblings(".hour").val();
+        // Save text in local storage
+        var tasks = [];
+        tasks [0] = time + text;
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        // var storedTasks = JSON.parse(localStorage.getItem("tasks"));
     })
-    // $("#" + timeKey).children().children().siblings();
-    console.log($("#" + timeKey).children().children().siblings())
-    localStorage.getItem(timeKey)
+   
+    function timeTracker() {
+        // get current number of hours.
+        var timeNow = moment().hour();
 
-    //use timeArray instead of timeKey
-}
-}
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
-init()
+            // To check the time and add the classes for background indicators
+            if (blockTime < timeNow) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else if (blockTime === timeNow) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    // Get item from local storage if any
+    $("#9am .textarea").val(localStorage.getItem("9am"));
+    $("#10am .textarea").val(localStorage.getItem("10am"));
+    $("#11am .textarea").val(localStorage.getItem("11am"));
+    $("#12pm .textarea").val(localStorage.getItem("12pm"));
+    $("#1pm .textarea").val(localStorage.getItem("1pm"));
+    $("#2pm .textarea").val(localStorage.getItem("2pm"));
+    $("#3pm .textarea").val(localStorage.getItem("3pm"));
+    $("#4pm .textarea").val(localStorage.getItem("4pm"));
+    $("#5pm .textarea").val(localStorage.getItem("5pm"));
+
+    timeTracker();
+
+    // var loadTasks = function() {
+    
+    //     var loadedTasks = localStorage.getItem("tasks");
+    //     var loadedTasks = JSON.parse(loadedTasks);
+        
+    //     console.log(loadedTasks)
+    
+    //     for (var i = 0; i < loadedTasks.length; i++) {
+    //         var text= loadedTasks[i].task
+    //         console.log(text)
+    //         var time = loadedTasks[i].part
+    //         $("#task"+ time).text(text)
+    //     }    
+    // }
+    
+    // loadTasks()
+    
+})
